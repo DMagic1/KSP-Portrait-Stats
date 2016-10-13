4445b97405d1722f773c6e1089752a907d3b63f2
DMagic <david.grandy@gmail.com>
2016-10-13 16:56:01 -0400

    New settings files and script for new game settings

---
+++ Source/PortraitStatsSettings.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using UnityEngine;
+namespace PortraitStats
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class PortraitStatsSettings : MonoBehaviour
+	{
+		[Persistent]
+		public bool ShowAlways = true;
+		[Persistent]
+		public bool UseIcon = true;
+		[Persistent]
+		public bool ExtendedTooltips = true;
+		[Persistent]
+		public bool HoverHighlight = true;
+		[Persistent]
+		public bool TransferButton = true;
+		[Persistent]
+		public int ReloadDelay = 5;
+		[Persistent]
+		public Color PilotColor = XKCDColors.PastelRed;
+		[Persistent]
+		public Color EngineerColor = XKCDColors.DarkYellow;
+		[Persistent]
+		public Color ScientistColor = XKCDColors.DirtyBlue;
+		[Persistent]
+		public Color TouristColor = XKCDColors.SapGreen;
+		[Persistent]
+		public Color UnknownColor = XKCDColors.White;
+
+		private const string fileName = "PluginData/Settings.cfg";
+		private string fullPath;
+		private StatsGameSettings settings;
+
+		private static bool loaded;
+		private static PortraitStatsSettings instance;
+
+		public static PortraitStatsSettings Instance
+		{
+			get { return instance; }
+		}
+
+		private void Awake()
+		{
+			if (loaded)
+				Destroy(gameObject);
+
+			DontDestroyOnLoad(gameObject);
+
+			loaded = true;
+
+			instance = this;
+
+			fullPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fileName).Replace("\\", "/");
+			GameEvents.OnGameSettingsApplied.Add(SettingsApplied);
+
+			if (Load())
+				PortraitStats.log("Settings file loaded");
+		}
+
+		private void OnDestroy()
+		{
+			GameEvents.OnGameSettingsApplied.Remove(SettingsApplied);
+		}
+
+		public void SettingsApplied()
+		{
+			if (HighLogic.CurrentGame != null)
+				settings = HighLogic.CurrentGame.Parameters.CustomParams<StatsGameSettings>();
+
+			if (settings == null)
+				return;
+
+			if (settings.UseAsDefault)
+			{
+				ShowAlways = settings.AlwaysShow;
+				UseIcon = settings.UseIcon;
+				ExtendedTooltips = settings.ExtendedTooltips;
+				HoverHighlight = settings.HoverHighlight;
+				TransferButton = settings.TransferButton;
+				ReloadDelay = settings.ReloadDelay;
+
+				if (Save())
+					PortraitStats.log("Settings file saved");
+			}
+		}
+
+		public bool Load()
+		{
+			bool b = false;
+
+			try
+			{
+				if (File.Exists(fullPath))
+				{
+					ConfigNode node = ConfigNode.Load(fullPath);
+					ConfigNode unwrapped = node.GetNode(GetType().Name);
+					ConfigNode.LoadObjectFromConfig(this, unwrapped);
+					b = true;
+				}
+				else
+				{
+					PortraitStats.log("Settings file could not be found [{0}]", fullPath);
+					b = false;
+				}
+			}
+			catch (Exception e)
+			{
+				PortraitStats.log("Error while loading settings file from [{0}]\n{1}", fullPath, e);
+				b = false;
+			}
+
+			return b;
+		}
+
+		public bool Save()
+		{
+			bool b = false;
+
+			try
+			{
+				ConfigNode node = AsConfigNode();
+				ConfigNode wrapper = new ConfigNode(GetType().Name);
+				wrapper.AddNode(node);
+				wrapper.Save(fullPath);
+				b = true;
+			}
+			catch (Exception e)
+			{
+				PortraitStats.log("Error while saving settings file from [{0}]\n{1}", fullPath, e);
+				b = false;
+			}
+
+			return b;
+		}
+
+		private ConfigNode AsConfigNode()
+		{
+			try
+			{
+				ConfigNode node = new ConfigNode(GetType().Name);
+
+				node = ConfigNode.CreateConfigFromObject(this, node);
+				return node;
+			}
+			catch (Exception e)
+			{
+				PortraitStats.log("Failed to generate settings file node...\n{0}", e);
+				return new ConfigNode(GetType().Name);
+			}
+		}
+		
+	}
+}
---
+++ Source/StatsGameSettings.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PortraitStats
+{
+	public class StatsGameSettings : GameParameters.CustomParameterNode
+	{
+		private string _title = "Portrait Stats";
+		private int _order = 0;
+		private string _section = "DMagic Mods";
+		private bool _haspresets = true;
+		private GameParameters.GameMode _gamemode = GameParameters.GameMode.ANY;
+
+		public StatsGameSettings()
+		{
+			if (HighLogic.LoadedScene == GameScenes.MAINMENU)
+			{
+				if (PortraitStatsSettings.Instance == null)
+					return;
+
+				AlwaysShow = PortraitStatsSettings.Instance.ShowAlways;
+				UseIcon = PortraitStatsSettings.Instance.UseIcon;
+				ExtendedTooltips = PortraitStatsSettings.Instance.ExtendedTooltips;
+				HoverHighlight = PortraitStatsSettings.Instance.HoverHighlight;
+				TransferButton = PortraitStatsSettings.Instance.TransferButton;
+				ReloadDelay = PortraitStatsSettings.Instance.ReloadDelay;
+			}
+		}
+
+		public override string Title
+		{
+			get { return _title; }
+		}
+
+		public override string Section
+		{
+			get { return _section; }
+		}
+
+		public override int SectionOrder
+		{
+			get { return _order; }
+		}
+
+		public override bool HasPresets
+		{
+			get { return _haspresets; }
+		}
+
+		public override GameParameters.GameMode GameMode
+		{
+			get { return _gamemode; }
+		}
+
+		public override void SetDifficultyPreset(GameParameters.Preset preset)
+		{
+			
+		}
+
+		public override bool Enabled(System.Reflection.MemberInfo member, GameParameters parameters)
+		{
+			if (member.Name == "Reload")
+				return HighLogic.LoadedSceneIsFlight;
+
+			return true;
+		}
+
+		[GameParameters.CustomStringParameterUI("", lines = 2, autoPersistance = false)]
+		public string Reload = "A re-load is required for changes to take effect.";
+		[GameParameters.CustomParameterUI("Always Show Info", toolTip = "Show trait and level info without hovering", autoPersistance = true)]
+		public bool AlwaysShow = true;
+		[GameParameters.CustomParameterUI("Use Trait Icon", toolTip = "Use icons in place of text for Kerbal class", autoPersistance = true)]
+		public bool UseIcon = true;
+		[GameParameters.CustomParameterUI("Use Expanded Tooltips", toolTip = "Show additional information in the portrait tooltips", autoPersistance = true)]
+		public bool ExtendedTooltips = true;
+		[GameParameters.CustomParameterUI("Highlight Current Part", toolTip = "Highlight the Kerbal's current part when hovering over their portrait", autoPersistance = true)]
+		public bool HoverHighlight = true;
+		[GameParameters.CustomParameterUI("Show Crew Transfer Button", toolTip = "Show crew transfer button below the standard IVA and EVA buttons", autoPersistance = true)]
+		public bool TransferButton = true;
+		[GameParameters.CustomIntParameterUI("Reload Delay", minValue = 3, maxValue = 20, toolTip = "May be modified if there are errors when adding or removing crew", autoPersistance = true)]
+		public int ReloadDelay = 5;
+		[GameParameters.CustomParameterUI("Use Settings As Default", toolTip = "Use the current settings as the defaults for all new games", autoPersistance = false)]
+		public bool UseAsDefault;
+	}
+}
